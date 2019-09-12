f24812f16d6d34ac59a8515ffd08e816de693a79
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-09-12 16:56:51 +0300

    Fix ByteChannel.readRemaining() content truncate

--- ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
+++ ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
@@ -6,7 +6,6 @@ package io.ktor.client.engine.ios
 
 import io.ktor.client.call.*
 import io.ktor.client.engine.*
-import io.ktor.client.engine.ios.toByteArray
 import io.ktor.client.request.*
 import io.ktor.client.utils.*
 import io.ktor.http.*
